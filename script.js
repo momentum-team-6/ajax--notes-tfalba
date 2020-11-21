d2538611fa4cdd9f53c452653772ce4216bd0d55
Tracy Falba <tfalba@mac.com>
2020-11-21 08:55:16 -0500

    started setting up grabbing edits and placing them to view in the sidebar

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const url = 'http://localhost:3000/notes/'
 
 const noteSave = document.querySelector('#save-note')
 const noteList = document.querySelector('#note-list')
+const sideBar = document.querySelector('#side-bar')
 
 fetch(url)
   .then(res => res.json())
@@ -48,13 +49,11 @@ function renderNote (todoObj) {
   const itemEl = document.createElement('div')
   const itemDetail = document.createElement('div')
   itemEl.classList.add('note-header')
-  itemEl.classList.add('delete')
   itemDetail.classList.add('note-body')
-  // debugger
+
   itemEl.id = todoObj.id
-  itemEl.innerHTML = `${todoObj.item}<i class='fas fa-times delete'></i><i class='fas fa-edit'></i>`
-  itemDetail.innerHTML = `${todoObj.detail}<br><br><br>Created at: ${todoObj.created_at}`
-  // use innerHTML if want to use a template literal
+  itemEl.innerHTML = `${todoObj.item}<i class='fas fa-times delete'></i><i class='fas fa-edit edit'></i>`
+  itemDetail.innerHTML = `${todoObj.detail}<br><br><br>${todoObj.created_at}`
   noteList.appendChild(itemEl)
   itemEl.appendChild(itemDetail)
 }
@@ -63,6 +62,7 @@ function deleteNote (eventTarget) {
   console.log(eventTarget.parentElement)
   const noteId = eventTarget.parentElement.id
   // debugger
+  alert ('Are you sure you want to delete this message? Skip the "OK" and reload page if not.')
   fetch(`http://localhost:3000/notes/${noteId}`, {
     method: 'DELETE'
   })
@@ -72,6 +72,7 @@ function deleteNote (eventTarget) {
     .then(function(data) {
       console.log(data)
     })
+  document.location.reload()
 }
 
 noteList.addEventListener('click', function (event) {
@@ -81,3 +82,49 @@ noteList.addEventListener('click', function (event) {
   }
 })
 
+noteList.addEventListener('click', function (event) {
+  if (event.target.classList.contains('edit')) {
+    editNote(event.target)
+  }
+})
+
+function renderEdit (todoObj) {
+  const itemEl = document.createElement('div')
+  const itemDetail = document.createElement('div')
+  itemEl.classList.add('note-header')
+  itemDetail.classList.add('note-body')
+
+  itemEl.id = todoObj.id
+  itemEl.innerHTML = todoObj.item
+  itemDetail.innerHTML = `${todoObj.detail}<br><br><br>Created at: ${todoObj.created_at}`
+  sideBar.appendChild(itemEl)
+  itemEl.appendChild(itemDetail)
+}
+
+function editNote (eventTarget) {
+  console.log(eventTarget)
+  console.log(eventTarget.parentElement)
+  const noteId = eventTarget.parentElement.id
+  console.log(noteId)
+
+fetch(`http://localhost:3000/notes/${noteId}`)
+.then(res => res.json())
+.then(data => {
+  renderEdit(data)
+})
+
+  // renderEdit (eventTarget.parentElement)
+  // debugger
+
+
+  // fetch(`http://localhost:3000/notes/${noteId}`, {
+  //   method: 'DELETE'
+  // })
+  //   .then(function(res) {
+  //     return res.json()
+  //   })
+  //   .then(function(data) {
+  //     console.log(data)
+  //   })
+  // document.location.reload()
+}
